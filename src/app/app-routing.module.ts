cce33bb7c5fff6bf32f4267bf1eb62b7abb203bb
AlfredoE89 <w0lf_0f_wall_street@icloud.com>
2019-12-03 10:30:13 -0600

    updated ionic user/delete/edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,43 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    path: "login",
+    loadChildren: () =>
+      import("./login/login.module").then(m => m.LoginPageModule)
   },
   {
-    path: 'logout',
-    loadChildren: () => import('./logout/logout.module').then( m => m.LogoutPageModule)
+    path: "logout",
+    loadChildren: () =>
+      import("./logout/logout.module").then(m => m.LogoutPageModule)
   },
   {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+    path: "register",
+    loadChildren: () =>
+      import("./register/register.module").then(m => m.RegisterPageModule)
   },
   {
-    path: 'users',
-    loadChildren: () => import('./users/users.module').then( m => m.UsersPageModule)
+    path: "users",
+    loadChildren: () =>
+      import("./users/users.module").then(m => m.UsersPageModule)
   },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
+  },
+  {
+    path: 'user-edit',
+    loadChildren: () => import('./user-edit/user-edit.module').then( m => m.UserEditPageModule)
+  },
+  {
+    path: 'user-delete',
+    loadChildren: () => import('./user-delete/user-delete.module').then( m => m.UserDeletePageModule)
+  }
 ];
 
 @NgModule({
--- src/app/users/users.page.ts
+++ src/app/users/users.page.ts
@@ -1,26 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { UsersService } from '../users.service';
-import { User } from '../user.model';
+import { Component, OnInit } from "@angular/core";
+import { UsersService } from "../users.service";
+import { User } from "../user.model";
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.page.html',
-  styleUrls: ['./users.page.scss'],
+  selector: "app-users",
+  templateUrl: "./users.page.html",
+  styleUrls: ["./users.page.scss"]
 })
 export class UsersPage implements OnInit {
-  user:User = new User();
+  user: User;
 
-  constructor(private usersService:UsersService) { }
+  constructor(private usersService: UsersService) {}
 
   ngOnInit() {
     this.getUsers();
   }
 
-  public getUsers(): void{
-    this.usersService.getUsers().subscribe(
-      (response:any) => {
-        console.log(response);
-      }
-    );
+  public getUsers(): void {
+    this.usersService.getUsers().subscribe((response: any) => {
+      this.user = response.users;
+    });
   }
-
 }
